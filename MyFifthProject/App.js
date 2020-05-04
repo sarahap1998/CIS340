1f91e67ced55385537e4229ef22ca0df08cc1269
sarahap1998 <63427459+sarahap1998@users.noreply.github.com>
2020-05-04 23:27:22 +0000

    Update App.js

--- MyFifthProject/App.js
+++ MyFifthProject/App.js
@@ -3,25 +3,13 @@ import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'baseline',
-      }}
-    >
-      <View
-        style={{
-          height: 50,
-          backgroundColor: 'powderblue',
-          alignSelf: 'stretch',
-        }}
-      ></View>
-      <View
-        style={{ width: 50, height: 50, backgroundColor: 'steelblue' }}
-      ></View>
-      <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}></View>
+    <View style={{flex: 1, 
+    flexDirection: 'column', 
+    justifyContent: 'center', 
+    alignitems: 'baseline'}}>
+      <View style={{height: 50, backgroundColor: 'red', alignSelf: 'stretch'}} />
+      <View style={{width: 50, height: 50, backgroundColor: 'yellow'}} />
+      <View style={{width: 50, height: 50, backgroundColor: 'green'}} />
     </View>
   );
 }
